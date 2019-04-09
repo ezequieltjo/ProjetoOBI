271a9e95d08a8706ca92aedf10032aa88801b823
Ezequiel Teotônio Jó <41303804+ezequieltjo@users.noreply.github.com>
2019-04-09 19:42:09 -0300

    Add files via upload

--- Frequencia.c
+++ Frequencia.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*int linhafrequente(int*a, b){
-
-
-}*/
-
 int main (void){
-    int n, q, x, r, k, t;
+    int n, q, x, r, k, t, primeiro;
     int cont1, cont2, cont3;
 
     scanf("%d", &n);
     scanf("%d", &q);
 
-    int arr1[n][q];
+    int arr1[n][n];
+    int arr2[n];
 
     for(cont1=0; cont1<q; cont1++){
         scanf("%d", &t);
@@ -22,7 +18,7 @@ int main (void){
             scanf("%d", &x);
             scanf("%d", &r);
 
-            for(cont2=0; cont2<=n; cont2++){
+            for(cont2=0; cont2<n; cont2++){
                 if(x>n) printf("\n error1\n");
                 arr1[x][cont2]=r;
             }
@@ -32,28 +28,34 @@ int main (void){
             scanf("%d", &x);
             scanf("%d", &r);
 
-            for(cont2=0; cont2<=n; cont2++){
+            for(cont2=0; cont2<n; cont2++){
                 if(x>n) printf("\n error2\n");
-                arr1[cont3][x]=r;
+                arr1[cont2][x]=r;
             }
-
-
         }
 
         if (t==3){
             scanf("%d", &x);
-            for(cont2=0; cont2<=n; cont2++){
+
+            for(cont2=0; cont2<n; cont2++){
                 if(x>n) printf("\n error3\n");
 
+                //primeiro = arr1[x][0];
 
+                for(cont3=0; cont<n; cont3++){
+                    if (arr1[x][cont2]==arr1[x][cont2+1]){
+                        arr2 =
 
+                    }
+
+                }
             }
 
 
         }
 
         if (t==4){
-            scanf("%d", &k);
+            scanf("%d", &x);
 
 
         }
